dc5ea661ad57e778b9133b86baf3567f289b1192
mirkogalizia <mirko.galizia@outlook.com>
2025-08-14 13:16:46 +0200

    push

--- src/lib/useAuth.ts
+++ src/lib/useAuth.ts
@@ -1,14 +1,16 @@
-"use client";
+// src/lib/useAuth.ts
+
+"use client"
 import { useEffect, useState } from "react";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./firebase"; // Assicurati di avere questo file
+import { onAuthStateChanged, User } from "firebase/auth";
+import { auth } from "./firebase";
 
 export function useAuth() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      setUser(user);
+    const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+      setUser(firebaseUser);
     });
     return () => unsubscribe();
   }, []);
